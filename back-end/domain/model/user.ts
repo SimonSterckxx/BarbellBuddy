f85f491f23d20ba1a35d802e6b87dba9f491d76b
Simon <simon.sterckx@student.ucll.be>
2024-01-31 18:38:04 +0100

    added user domain class

---
+++ back-end/domain/model/user.ts
@@ -0,0 +1,15 @@
+import { Role } from "../../types";
+
+export class User {
+    readonly id?: number;
+    readonly username: string;
+    readonly password: string;
+    readonly role: Role
+
+    constructor(user: { id?: number, username: string, password: string, role: Role }) {
+        this.id = user.id
+        this.username = user.username
+        this.password = user.password
+        this.role = user.role
+    }
+}
---
+++ back-end/types/index.ts
@@ -0,0 +1,1 @@
+export type Role = 'Trainee' | 'Coach' | 'Admin';
