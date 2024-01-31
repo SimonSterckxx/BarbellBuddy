420628712831c0098e106edb2818748cbf788214
Simon <simon.sterckx@student.ucll.be>
2024-01-31 18:51:09 +0100

    added coach class

---
+++ back-end/domain/model/coach.ts
@@ -0,0 +1,16 @@
+import { Role } from "../../types";
+import { Trainee } from "./trainee";
+import { User } from "./user";
+
+export class Coach extends User {
+    readonly firstName: string
+    readonly lastName: string
+    readonly trainees?: Trainee[]
+
+    constructor(user: User, coach: { firstName: string, lastName: string, trainees: Trainee[] }) {
+        super(user)
+        this.firstName = coach.firstName
+        this.lastName = coach.lastName
+        this.trainees = coach.trainees
+    }
+}
